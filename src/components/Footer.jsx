e18f71aa7185126ab727fa95008c9d726aeca00e
Md. Muttalib <mdabdulmottalib2222@gmail.com>
2025-11-10 07:42:25 +0600

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import logo from "../assets/logo-home-service.png";
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+   <>
+    <div className="bg-[#021f2a] text-white py-10">
+      <div className="flex justify-evenly items-start">
+        <div className=" space-y-3">
+          <div className="flex items-center space-y-3">
+            <img className="w-20" src={logo} alt="" />
+            <h3 className="text-2xl italic font-bold">ServEase</h3>
+          </div>
+          <p className="text-gray-400">
+            We provide the best home repair <br /> services with reliable care.
+          </p>
+        </div>
+        <div className=" space-y-3">
+          <h4 className="font-bold">Office</h4>
+          <p className="text-gray-400">
+            Corporate Office- 175 24th Street,
+            <br /> OT- 35 London, UK 265
+          </p>
+          <div className="space-y-2">
+            <p> servease@gmail.com</p>
+            <p>+13-1454656</p>
+          </div>
+        </div>
+
+        <div className=" space-y-3">
+          <h4 className="font-bold">Links</h4>
+          <ul className="text-gray-400 space-y-3">
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Services</a>
+            </li>
+            <li>
+              <a href="">Project</a>
+            </li>
+            <li>
+              <a href="">Pricing</a>
+            </li>
+            <li>
+              <a href="">FQA</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className=" space-y-3">
+          <h4 className="font-bold">Get in Touch</h4>
+          <ul className="text-gray-400 space-y-3">
+            <li className="flex gap-2">
+              <a href="">
+                <FaFacebook className="text-white"></FaFacebook>{" "}
+              </a>{" "}
+              <p>Facebook</p>
+            </li>
+            <li className="flex gap-2">
+              <a href="">
+                <FaInstagram className="text-white"></FaInstagram>{" "}
+              </a>
+              <p>Instagram</p>
+            </li>
+            <li className="flex gap-2">
+              <a href="">
+                <FaLinkedin className="text-white"></FaLinkedin>
+              </a>
+              <p>Linkedin</p>
+            </li>
+            <li className="flex gap-2">
+              <a href="">
+                <FaXTwitter className="text-white"></FaXTwitter>
+              </a>
+              <p>Twitter</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+     <div className="bg-[#021f2a] px-10 py-3 ">
+       <div className=" flex justify-between items-center border-t border-blue-400 py-3">
+         <p className="text-gray-400 text-[.8rem]">© 2025 Shromik. All rights reserved.</p>
+        <p className="text-gray-400 text-[.8rem]">Privacy Policy | Terms & Conditions</p>
+       </div>
+      </div>
+   </>
+  );
+};
+
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React from "react";
+import logo from "../assets/logo-home-service.png";
+import { NavLink } from "react-router";
 
 const Navbar = () => {
-        return (
-                <div>
-                        
-                        
-                </div>
-        );
+  return (
+    <div className="bg-[#021f2a] text-white flex justify-between items-center py-4 px-5 mt-3">
+      <div className="flex  items-center">
+        <img className="w-20" src={logo} alt="" />
+        <h3 className="text-2xl italic font-bold">ServEase</h3>
+      </div>
+      <ul className="flex gap-5">
+        <li>
+          <NavLink>Home</NavLink>
+        </li>
+        <li>
+          <NavLink>Services</NavLink>
+        </li>
+        <li>
+          <NavLink>My Services</NavLink>
+        </li>
+        <li>
+          <NavLink>Add Service</NavLink>
+        </li>
+        <li>
+          <NavLink>My Bookings</NavLink>
+        </li>
+        <li>
+          <NavLink>Profile</NavLink>
+        </li>
+      </ul>
+
+      <div className="space-x-3 ">
+        <button className="btn shadow-none bg-[#021f2a] border-blue-500 text-white border-color-primary">Login</button>
+        <button className="btn shadow-none bg-[#021f2a] border-blue-500 text-white border-color-primary">Register</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
